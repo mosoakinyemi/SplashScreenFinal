3b68997cacd0bad1a2b3131ca1e89d9e2eddb5be
Moso Akinyemi <akinyemimosolasi@gmail.com>
2020-02-11 11:55:19 +0100

    added constraint files from example proj.

---
+++ ios/SplashScreenFinal/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AnimatedGifLaunchScreen-Example
+//
+//  Created by Amer Hukic on 13/09/2018.
+//  Copyright © 2018 Amer Hukic. All rights reserved.
+//
+
+import UIKit
+import SwiftyGif
+
+class ViewController: UIViewController {
+
+    let logoAnimationView = LogoAnimationView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(logoAnimationView)
+        logoAnimationView.pinEdgesToSuperView()
+        logoAnimationView.logoGifImageView.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logoAnimationView.logoGifImageView.startAnimatingGif()
+    }
+
+}
+
+extension ViewController: SwiftyGifDelegate {
+    func gifDidStop(sender: UIImageView) {
+        logoAnimationView.isHidden = true
+    }
+}
+
