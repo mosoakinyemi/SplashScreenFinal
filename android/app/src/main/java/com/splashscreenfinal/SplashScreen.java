803df85e5a90082144466f9973d2ee79dc5590fa
Moso Akinyemi <akinyemimosolasi@gmail.com>
2020-02-12 11:31:05 +0100

    Android Splash Aligned

--- android/app/src/main/java/com/splashscreenfinal/SplashScreen.java
+++ android/app/src/main/java/com/splashscreenfinal/SplashScreen.java
@@ -7,48 +7,44 @@ import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.airbnb.lottie.LottieAnimationView;
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class SplashScreen extends AppCompatActivity {
-    private LottieAnimationView topAnimation;
-    private LottieAnimationView bottomAnimation;
 
-    //    private GifImageView topWaveAnimation;
-//    private GifImageView bottomWaveAnimation;
+        private GifImageView topAnimation;
+        private GifImageView bottomAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
-        topAnimation =(LottieAnimationView) findViewById(R.id.firstAnimation);
-        topAnimation.setAnimation("top_bands.json");
 
-        bottomAnimation =(LottieAnimationView) findViewById(R.id.bottomAnimation);
-        bottomAnimation.setAnimation("bottom_bands.json");
-        topAnimation.playAnimation();
-        bottomAnimation.playAnimation();
+        bottomAnimation =(GifImageView) findViewById(R.id.bottomAnimation);
+        topAnimation =(GifImageView) findViewById(R.id.topAnimation);
 
 
-//        cameraAnimation =(GifImageView) findViewById(R.id.cameraAnimation);
-//        topWaveAnimation =(GifImageView) findViewById(R.id.topWaveAnimation);
-//        cameraAnimation =(GifImageView) findViewById(R.id.cameraAnimation);
+        try{
+            InputStream inputStream1 = getAssets().open("top_bands_final.gif");
+            byte[] bytes1 = IOUtils.toByteArray(inputStream1);
+            topAnimation.setBytes(bytes1);
+            topAnimation.startAnimation();
 
+            //For topWaveAnimation
+            InputStream inputStream2 = getAssets().open("bottom_bands_final.gif");
+            byte[] bytes2 =IOUtils.toByteArray(inputStream2);
+            bottomAnimation.setBytes(bytes2);
+            bottomAnimation.startAnimation();
+        }
+        catch (IOException ex){
 
-//        try{
-//            InputStream inputStream1 = getAssets().open("camera.gif");
-//            byte[] bytes1 =IOUtils.toByteArray(inputStream1);
-//            cameraAnimation.setBytes(bytes1);
-//            cameraAnimation.startAnimation();
-//
-//            //For topWaveAnimation
-//            InputStream inputStream2 = getAssets().open("top_wave.gif");
-//            byte[] bytes2 =IOUtils.toByteArray(inputStream2);
-//            topWaveAnimation.setBytes(bytes2);
-//            topWaveAnimation.startAnimation();
-//        }
-//        catch (IOException ex){
-//
-//        }
+        }
 
         //Wait for 3 seconds before going to React Native App
         new Handler().postDelayed(new Runnable() {
@@ -56,7 +52,6 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 SplashScreen.this.startActivity(new Intent(SplashScreen.this , MainActivity.class));
                  SplashScreen.this.finish();
-//                overridePendingTransition(R.anim.fadein,R.anim.fadeout);
 
             }
         }, 3000 );
