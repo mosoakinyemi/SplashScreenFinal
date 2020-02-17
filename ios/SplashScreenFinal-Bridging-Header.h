3dbb4f9154931808e3282a32ded30a852d6d7cb2
Moso Akinyemi <akinyemimosolasi@gmail.com>
2020-02-17 11:30:27 +0100

    Reverted before trying Code Merge

---
+++ ios/SplashScreenFinal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreenFinal-Bridging-Header.h
+//  SplashScreenFinal
+//
+//  Created by Moso's MacBook on 17/02/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef SplashScreenFinal_Bridging_Header_h
+#define SplashScreenFinal_Bridging_Header_h
+
+
+#endif /* SplashScreenFinal_Bridging_Header_h */
--- ios/SplashScreenFinal/AppDelegate.m
+++ ios/SplashScreenFinal/AppDelegate.m
@@ -13,6 +13,57 @@
 
 @implementation AppDelegate
 
+//
+//- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+//{
+//
+//   UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"AnimationView" bundle:[NSBundle mainBundle]];
+//   UIViewController *vc =[storyboard instantiateInitialViewController];
+//
+//   // Set root view controller and make windows visible
+//   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+//   self.window.rootViewController = vc;
+//   [self.window makeKeyAndVisible];
+//
+//   return YES;
+//}
+
+// -(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+//{
+//    if (true) {
+//        // To-do, show logged in view
+//    } else {
+//        // No, display the login page.
+//        [self showLoginView];
+//    }
+//    return YES;
+//}
+//- (void)showAnimatedView
+//{
+//    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+//    AnimatedViewController *animatedViewController = [storyboard instantiateViewControllerWithIdentifier:@"AnimatedViewController"];
+//    [self.window makeKeyAndVisible];
+//    [self.window.rootViewController presentViewController:animatedViewController animated:YES completion:NULL];
+//}
+
+
+//-----Trial 2-----//
+//- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+//
+//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"AnimationView" bundle:[NSBundle mainBundle]];
+//    AnimationViewController *initialVC = [storyboard instantiateViewControllerWithIdentifier:@"AnimationViewController"];
+//    self.window.rootViewController = initialVC;
+//    [self.window makeKeyAndVisible];
+//
+//    ShowThisViewController *showThisVC = [storyboard instantiateViewControllerWithIdentifier:@"ShowThisViewController"];
+//    [self.window.rootViewController presentViewController:showThisVC animated:YES completion:nil];
+//
+//    return YES; }
+
+
+//-----Original Codes--------//
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
