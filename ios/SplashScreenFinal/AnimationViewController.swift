0bff036eafa6d52c91ce7d7a6982e9603cc0879c
Moso Akinyemi <akinyemimosolasi@gmail.com>
2020-05-06 08:06:45 +0100

    Attempting resizing of animation

--- ios/SplashScreenFinal/AnimationViewController.swift
+++ ios/SplashScreenFinal/AnimationViewController.swift
@@ -6,13 +6,22 @@ import Lottie
 
   
 
-  @IBOutlet weak var BottomAnimationView: UIView!
+//  @IBOutlet weak var BottomAnimationView: UIView!
+  @IBOutlet var ContainerView: UIView!
   @IBOutlet weak var TopAnimationView: UIView!
   
   override func viewDidLoad() {
         super.viewDidLoad()
-    let topLottie =  AnimationView(name: "top_bands")
-    TopAnimationView.contentMode = .scaleAspectFill
+    let imageView = AnimationView()
+
+      let topLottie =  AnimationView(name: "whole_splash")
+
+         let aspect =  414/896
+    self.aspectConstraint = NSLayoutConstraint(item: self.TopAnimationView, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.TopAnimationView, attribute: NSLayoutConstraint.Attribute.height, multiplier: CGFloat(aspect), constant: 1.2)
+
+
+//    let topLottie =  AnimationView(name: "whole_splash")
+    TopAnimationView.contentMode = .scaleToFill
     self.TopAnimationView.addSubview(topLottie)
     topLottie.frame = self.TopAnimationView.bounds
     topLottie.animationSpeed = 1
@@ -20,15 +29,33 @@ import Lottie
     topLottie.play()
     
 //    Bottom animation codes
-    let bottomLottie =  AnimationView(name: "br_band")
-    BottomAnimationView.contentMode = .scaleAspectFill
-    self.BottomAnimationView.addSubview(bottomLottie)
-    bottomLottie.frame = self.BottomAnimationView.bounds
-    bottomLottie.animationSpeed = 1
-//    bottomLottie.loopMode = .loop
-    bottomLottie.play()
+//    let bottomLottie =  AnimationView(name: "br_band")
+//    BottomAnimationView.contentMode = .scaleAspectFill
+//    self.BottomAnimationView.addSubview(bottomLottie)
+//    bottomLottie.frame = self.BottomAnimationView.bounds
+//    bottomLottie.animationSpeed = 1
+////    bottomLottie.loopMode = .loop
+//    bottomLottie.play()
 
     }
-    
+
+  var aspectConstraint: NSLayoutConstraint? {
+
+  willSet {
+
+      if((aspectConstraint) != nil) {
+          self.TopAnimationView.removeConstraint(self.aspectConstraint!)
+      }
+  }
+
+  didSet {
+
+      if(aspectConstraint != nil) {
+          self.TopAnimationView.addConstraint(self.aspectConstraint!)
+      }
+
+  }
+  }
+
 
 }
